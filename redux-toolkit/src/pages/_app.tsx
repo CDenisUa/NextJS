b1300f8a19f1b05ba2fd23bf66fd78f7de28660e
Denys Chepiha <dchepiha@exoft.net>
2022-06-06 09:27:53 +0200

    Initialize project

---
+++ redux-toolkit/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+// Types
+import type { AppProps } from 'next/app';
+// Styles
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
