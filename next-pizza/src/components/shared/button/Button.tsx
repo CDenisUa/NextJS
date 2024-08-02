de34ae8f65c91bed4025b86072038c5ef268e4d5
Denys <chepigadeveloper@gmail.com>
2024-08-02 23:24:23 +0200

    Add component button

---
+++ next-pizza/src/components/shared/button/Button.tsx
@@ -0,0 +1,12 @@
+// Core
+import { FC } from 'react';
+// Styles
+import styles from './styles.module.css';
+
+const Button: FC = () => {
+    return (
+        <div>Button</div>
+    );
+}
+
+export default Button;
