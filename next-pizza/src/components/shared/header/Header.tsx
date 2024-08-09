252dd7d9df4842e85904cbcd9838c7d58020d24c
Denys <chepigadeveloper@gmail.com>
2024-08-09 10:41:25 +0200

    Finish Header component

--- next-pizza/src/components/shared/header/Header.tsx
+++ next-pizza/src/components/shared/header/Header.tsx
@@ -2,7 +2,11 @@
 import {FC} from 'react';
 import Image from 'next/image';
 // Images
-import {User} from "lucide-react";
+import {
+    ShoppingCart,
+    User,
+    ArrowRight
+} from "lucide-react";
 // Utils
 import {cn} from "@/lib/utils";
 // Components
@@ -27,10 +31,27 @@ export const Header: FC = () => {
                 </div>
 
                 <div className='flex items-center gap-3'>
-                    <Button variant='outline'>
-                        <User size={16} />
+                    <Button
+                        variant='outline'
+                        className='flex items-center gap-1'
+                    >
+                        <User size={16}/>
                         Log In
                     </Button>
+
+                    <div>
+                        <Button className='group relative'>
+                            <b>10$</b>
+                            <span className='h-full w-[1px] bg-white/30 mx-3' />
+                            <div
+                                className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'
+                            >
+                                <ShoppingCart className='h-4 w-4 relative' strokeWidth={2} />
+                                <b>3</b>
+                            </div>
+                            <ArrowRight size={16} className='w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0' />
+                        </Button>
+                    </div>
                 </div>
             </Container>
         </header>
