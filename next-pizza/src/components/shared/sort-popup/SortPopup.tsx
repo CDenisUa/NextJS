230f65edbf0d7ec5bea876d8f7b374a728aba67f
Denys <chepigadeveloper@gmail.com>
2024-08-10 19:01:56 +0200

    Finish SortPopup component

--- next-pizza/src/components/shared/sort-popup/SortPopup.tsx
+++ next-pizza/src/components/shared/sort-popup/SortPopup.tsx
@@ -1,10 +1,24 @@
 // Core
-import { FC } from 'react';
-// Styles
-import styles from './styles.module.css';
+import {FC} from 'react';
+// Images
+import {ArrowUpDown} from "lucide-react";
+// Utils
+import {cn} from "@/lib/utils";
+// Components
+import {SortPopupPropTypes} from "@/components/shared/sort-popup/SortPopup.types";
 
-export const SortPopup: FC = () => {
+export const SortPopup: FC<SortPopupPropTypes> = (props) => {
+    const {className} = props;
     return (
-        <div>SortPopup</div>
+        <div
+            className={cn(
+                'inline-flex items-center gap-1 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer',
+                className
+            )}
+        >
+            <ArrowUpDown/>
+            <b>Sort:</b>
+            <b className='text-primary'>Popular</b>
+        </div>
     );
 }
---
+++ next-pizza/src/components/shared/sort-popup/SortPopup.types.ts
@@ -0,0 +1,3 @@
+export interface SortPopupPropTypes {
+    className?: string
+}
