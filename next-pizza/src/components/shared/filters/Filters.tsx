6d2957bf4e726695ab603c70e59c2c7c4c61463e
Denys <chepigadeveloper@gmail.com>
2024-08-11 21:43:08 +0200

    Initialize Filters component

---
+++ next-pizza/src/components/shared/filters/Filters.tsx
@@ -0,0 +1,12 @@
+// Core
+import { FC } from 'react';
+// Styles
+import styles from './styles.module.css';
+
+const Filters: FC = () => {
+    return (
+        <div>Filters</div>
+    );
+}
+
+export default Filters;
