e4d80be93f0ba634cce7af7ec71e91d5efca5ecd
Denys <chepigadeveloper@gmail.com>
2024-08-17 22:13:49 +0200

    Add test => Renders RangeSlider with default props

--- next-pizza/setupTests.ts
+++ next-pizza/setupTests.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+import ResizeObserver from 'resize-observer-polyfill';
+
+global.ResizeObserver = ResizeObserver;
+
+beforeAll(() => {
+    Element.prototype.hasPointerCapture = () => false;
+    Element.prototype.setPointerCapture = () => {};
+});
---
+++ next-pizza/src/components/ui/range-slider/RangeSlider.test.tsx
@@ -0,0 +1,23 @@
+// Core
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+// Components
+import { RangeSlider } from './RangeSlider';
+
+describe('RangeSlider component', () => {
+    const defaultProps = {
+        min: 0,
+        max: 100,
+        step: 1,
+        value: [20, 80],
+        onValueChange: jest.fn(),
+        formatLabel: (value: string | number) => `${value}%`,
+    };
+
+    test('Renders RangeSlider with default props', () => {
+        render(<RangeSlider {...defaultProps} />);
+        const firstSlider = screen.getAllByRole('slider')[0];
+        expect(firstSlider).toBeInTheDocument();
+    });
+});
